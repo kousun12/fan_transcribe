58e636c958938459c0f26c5f785eaf24c4cdcdbf
Rob Cheung <kousun12@gmail.com>
2023-03-19 22:21:19 -0400

    --wip-- [skip ci]

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ from logger import log
 
 
 class APIArgs(BaseModel):
-    url: str
+    url: Union[str, None] = None
     summarize: Union[bool, None] = None
     callback_url: Union[str, None] = None
     byte_string: Union[str, None] = None
@@ -25,26 +25,28 @@ class APIArgs(BaseModel):
     secret=modal.Secret.from_name("api-secret-key"),
 )
 def transcribe(api_args: APIArgs, x_modal_secret: str = Header(default=None)):
+    log.info(f"Processing {api_args.url}")
     secret = os.environ["API_SECRET_KEY"]
     if secret and x_modal_secret != secret:
         return {"error": "Not authorized"}
+
+    overrides = {}
+    if api_args.url:
+        overrides["url"] = api_args.url
+    if api_args.byte_string:
+        overrides["filename"] = f"bytes-{int(time.time())}.mp3"
+
     if api_args.callback_url:
         results = FanTranscriber.queue(
             api_args.callback_url,
-            cfg=WEB_DEFAULT_ARGS.merge({"url": api_args.url}),
+            cfg=WEB_DEFAULT_ARGS.merge(overrides),
             metadata=api_args.callback_metadata,
             summarize=bool(api_args.summarize),
         )
         return {"call_id": results.object_id}
 
-    log.info(f"Processing {api_args.url}")
     results = FanTranscriber.run(
-        {
-            "url": api_args.url,
-            "filename": f"bytes-{int(time.time())}.mp3"
-            if api_args.byte_string
-            else None,
-        },
+        overrides,
         byte_string=api_args.byte_string,
     )
     return results
